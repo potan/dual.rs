b31861b3dbe60e11d197c597040c3a74e19cb14c
Mike Potanin <potanin@gmail.com>
2014-10-10 16:06:17 +0400

    Newton's method example.

---
+++ newton.rs
@@ -0,0 +1,21 @@
+
+#![feature(globs)]
+
+extern crate dual;
+use dual::Dual;
+
+use std::num::*;
+
+fn newton<F:Float>(f: |Dual<F>| -> Dual<F>, s:F, eps:F) -> F {
+ let Dual{ val:fx, der:dx } = f(Dual{ val:s, der:One::one() });
+ if fx.abs() < eps {
+  s
+ } else {
+  newton(f, s - fx/dx, eps)
+ }
+}
+
+fn main() {
+ let two:Dual<f64> = Dual{ val:2.0, der:0.0};
+ println!("{}", newton(|x:Dual<f64>| { x*x - two}, 2.0, 0.0000000001));
+}
